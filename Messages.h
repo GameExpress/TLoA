4ddbcdc62ecca92b3436c04718c900773e113750
GameExpress <minecraftpika695@gmail.com>
2018-05-20 07:47:19 +0200

    Add files via upload

---
+++ Messages.h
@@ -0,0 +1,55 @@
+#ifndef messages_h
+#define messages_h
+
+uint8_t messageDuration;
+String message;
+
+void createFoundLootMessage(uint8_t t) {
+  messageDuration = 60;
+  switch (t) {
+    case 0:
+      message = "You found coins.";
+      break;
+    case 1:
+      message = "You found gems.";
+      break;
+    case 2:
+      message = "You found a key.";
+      break;
+    case 3:
+      message = "You got a heart.";
+      break;
+    case 4:
+      message = "You obtained:   \nUnbreaking";
+      break;
+    case 5:
+      message = "You obtained:   \nMending";
+      break;
+    case 6:
+      message = "You obtained:   \nHeart boost";
+      break;
+    case 7:
+      message = "You acquired:   \nSwimming";
+      break;
+    case 8:
+      message = "You acquired:   \nSword Melting";
+      break;
+    case 9:
+      message = "You acquired:   \nTeleportation";
+  }
+}
+
+void createMessage(String m) {
+  messageDuration = 60;
+  message = m;
+}
+
+void displayMessage() {
+  if(messageDuration > 0) {
+    messageDuration--;
+    arduboy.setCursor(0,0);
+    arduboy.print(message);
+  }
+}
+
+#endif
---
+++ TheLegendOfArdus.ino
@@ -0,0 +1,36 @@
+//The Legend of Ardus
+//Version 0.1
+
+//License:
+//This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+//To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+#include <Arduboy2.h>
+Arduboy2 arduboy;
+#include "Gameplay.h"
+
+/* BREAK IN CASE OF EMERGENCY!!! (out of memory)
+int main() {
+  init();
+  setup();
+  while(true) {
+    loop();
+  }
+  return 0;
+}
+*/
+void setup() {
+  arduboy.begin();
+  arduboy.clear();
+  arduboy.setFrameRate(30);
+  //Serial.begin(9600);
+  loadGame();
+  randomSeed(player.coins + player.gems + player.keys + opened[0] * 2 + opened[3] * 7);
+}
+void loop() {
+  if(!(arduboy.nextFrame())) return;
+  arduboy.pollButtons();
+  game();
+  //Serial.write(arduboy.getBuffer(), 128 * 64 / 8);
+  arduboy.display(true);
+}
